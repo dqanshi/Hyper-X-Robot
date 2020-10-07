df4c1fdd27e10eff9b4221f4def8f234754edf24
D'Rock <62856832+santo-surviver@users.noreply.github.com>
2020-10-07 13:04:53 +0530

    Update panel_string.py

--- tg_bot/modules/helper_funcs/panel_string.py
+++ tg_bot/modules/helper_funcs/panel_string.py
@@ -1,21 +1,25 @@
-ALLUKA_IMG = "https://telegra.ph/file/1308fbf433b0f02abaae6.jpg"
-
+if INTRO_IMG is None:
+    HYPER_X _IMG = "https://telegra.ph/file/e1e804f82a68529627555.jpg"
+else:
+  HYPER_X_IMG = INTRO_IMG    
 SOURCE_STRING = """
-I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/anilchauhanxda/allukabot)
+✯Im built-in python 3 to help and manage groups
+✯To clone me [𝘾𝙡𝙞𝙘𝙠 𝙝𝙚𝙧𝙚](https://github.com/santo-surviver/Hyper-X-Robot)
+✯For bot updates [𝙅𝙤𝙞𝙣 𝙃𝙚𝙧𝙚](https://t.me/Universal_Blazer)
 """
 
 HELP = """
-Hey there! My name is *{}*.
+𝑯𝒆𝒚 𝑰𝒂𝒎  *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with.
-*Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /source: Information about my source.
- - /settings:
-  - in PM: will send you your settings for all supported modules.
-  - in a group: will redirect you to pm, with all that chat's settings.
+the things I can help you with
+✰𝙈𝙖𝙞𝙣 𝙖𝙫𝙖𝙞𝙡𝙖𝙗𝙡𝙚 𝙘𝙤𝙢𝙢𝙖𝙣𝙙𝙨✰:
+ ➪ /start: start the bot
+ ➪ /help: PM's you this message.
+ ➪ /help <module name>: PM's you info about that module.
+ ➪ /source: Information about my source.
+ ➪ /settings:
+  » in PM: will send you your settings for all supported modules.
+  » in a group: will redirect you to pm, with all that chat's settings.
 {}
 And the following:
 """
